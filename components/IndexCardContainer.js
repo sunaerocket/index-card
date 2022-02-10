60a490fcb8c4b1a14f43180d5bbffb5590cdc5c1
sunaerocket <sunaerocket@gmail.com>
2022-02-10 15:25:58 +0900

    인덱스 참조 오류 수정

--- components/IndexCardContainer.js
+++ components/IndexCardContainer.js
@@ -3,34 +3,6 @@ import React, { useState } from 'react';
 import IndexCard from './IndexCard';
 import Navigation from './Navigation';
 
-export default function IndexCardContainer({ cards }) {
-  const [index, setIndex] = useState(0);
-
-  const handleNextClick = () => {
-    if (index >= cards.length - 1) {
-      return;
-    }
-
-    setIndex(index + 1);
-  };
-
-  const handlePrevClick = () => {
-    if (index <= 0) {
-      return;
-    }
-
-    setIndex(index - 1);
-  };
-
-  const progress = {
-    current: index + 1,
-    total: cards.length,
-  };
-
-  return (
-    <>
-      <IndexCard card={cards[index]} />
-      <Navigation progress={progress} onClickPrev={handlePrevClick} onClickNext={handleNextClick} />
-    </>
-  );
+export default function IndexCardContainer({ cards, currentIndex }) {
+  return <IndexCard card={cards[currentIndex]} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from 'next/head';
 
 import IndexCardContainer from '../components/IndexCardContainer';
 import TagContainer from '../components/TagContainer';
+import Navigation from '../components/Navigation';
 
 import { createCards, getAllTags } from '../lib/createCards';
 
@@ -12,11 +13,34 @@ import styles from '../styles/Home.module.css';
 export default function Home({ cards, tags }) {
   const [currentTag, setCurrentTag] = useState('all');
   const [currentCards, setCurrentCards] = useState(cards);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const progress = {
+    current: currentIndex + 1,
+    total: currentCards.length,
+  };
 
   const handleTagClick = (tag) => {
     setCurrentTag(tag);
     const filteredCards = tag === 'all' ? cards : cards.filter((card) => card.tags.includes(tag));
     setCurrentCards(filteredCards);
+    setCurrentIndex(0);
+  };
+
+  const handleNextClick = () => {
+    if (currentIndex >= cards.length - 1) {
+      return;
+    }
+
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const handlePrevClick = () => {
+    if (currentIndex <= 0) {
+      return;
+    }
+
+    setCurrentIndex(currentIndex - 1);
   };
 
   return (
@@ -30,7 +54,8 @@ export default function Home({ cards, tags }) {
       <main className={styles.main}>
         <TagContainer tags={tags} currentTag={currentTag} onTagClick={handleTagClick} />
         <div className={styles.grid}>
-          <IndexCardContainer cards={currentCards} />
+          <IndexCardContainer cards={currentCards} currentIndex={currentIndex} />
+          <Navigation progress={progress} onClickPrev={handlePrevClick} onClickNext={handleNextClick} />
         </div>
       </main>
     </div>
